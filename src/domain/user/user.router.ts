8d75e24f47ed875b9e1b3fc6328b9ccf405b8928
Maciej Krawczyk <maciej.krawczyk@stxnext.pl>
2022-01-24 11:48:36 +0100

    request body validation middleware

--- src/domain/user/user.router.ts
+++ src/domain/user/user.router.ts
@@ -1,10 +1,10 @@
 import { RouteParams, Router } from "oak";
 import { CreateUserController } from "./controllers/create-user.controller.ts";
 import { GetUsersController } from "./controllers/get-users.controller.ts";
-import { userErrorHandler } from "./utils/user.error-handler.ts";
 import { AppState } from "../../types/state.ts";
 import { DeleteUserController } from "./controllers/delete-user.controller.ts";
-
+import { validatorMiddleware } from "../../shared/middlewares/validation.middleware.ts"
+import { CreateUserValidation } from "./validators/create-user.validator.ts"
 
 export const userRouter = new Router<RouteParams, Partial<AppState>>();
 
@@ -13,7 +13,6 @@ userRouter
     ctx.state.logger!.debug("User router");
     return next();
   })
-  .use(userErrorHandler)
   .get("/users", new GetUsersController().handle)
-  .post("/user", new CreateUserController().handle)
+  .post("/user", validatorMiddleware<AppState>(CreateUserValidation), new CreateUserController().handle)
   .delete("/user", new DeleteUserController().handle)
